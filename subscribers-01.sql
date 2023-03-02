ef5d619f1c880d8e64aa6b4a0a04d13d177900b6
PaulineCurt <curtpauline@gmail.com>
2023-03-02 14:14:40 +0100

    import BDD & primary key email

--- subscribers-01.sql
+++ subscribers-01.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 02, 2023 at 01:07 PM
+-- Generation Time: Mar 02, 2023 at 01:12 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
